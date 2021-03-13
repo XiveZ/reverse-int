d2dec33a6c525d24bed4c9cb17a671ffa2cd8e63
Nj <jumrsup@hi2.in>
2021-03-13 17:02:43 +0300

    feat: added code for calculation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+
+    if(n.toString().includes('-')) return parseInt(n.toString().slice(1,n.length).split('').reverse().join(''))
+
+    if(parseInt(n.toString().substring(n.toString().length-1, n.toString().length)) == 0 ) return parseInt(n.toString().slice(0, n.toString().indexOf('0')).split('').reverse().join(''))
+
+    return parseInt(n.toString().split('').reverse().join(''));
 }
